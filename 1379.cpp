ff4eb0229446ea9ef88dd7ce7907001f8f527b35
Imran Sihab <imran.sihab.0@gmail.com>
2024-02-08 05:11:23 +0600

    URI Solutions
    at [ 2/8/24, 4:43:00 AM ]

---
+++ 1379.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n, m;
+    while(cin >> n >> m){
+        if(n == 0 && m == 0){
+            break;
+        }
+
+        cout << 2 * n - m << endl;
+
+    }
+    
+return 0;
+}
