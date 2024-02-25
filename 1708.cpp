48dc72aa20281c7a9024b6f9496c8975186929df
Imran Sihab <imran.sihab.0@gmail.com>
2024-02-25 18:28:12 +0600

    URI Solutions
    at [ 2/25/24, 6:25:52 PM ]

---
+++ 1708.cpp
@@ -0,0 +1,14 @@
+// https://judge.beecrowd.com/en/problems/view/1244
+// author: @imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int a, b, t = 0;
+    cin >> a >> b;
+
+    cout << ceil(b/(b-a)) << endl;
+    
+return 0;
+}
